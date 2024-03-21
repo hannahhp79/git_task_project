eab502b80da0f1908dc5699137d3a9c793d0af34
hannahhp <ftt.tuan.phamvan@grabtaxi.com>
2024-03-21 18:57:01 +0800

    Change script to accept input from user then print out

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-print("Git is awesome!")
+# Accept input from the user
+user_input = input("Enter something: ")
+
+# Print out the user's input
+print("You entered:", user_input)
